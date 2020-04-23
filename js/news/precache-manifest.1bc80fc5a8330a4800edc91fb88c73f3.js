4a9329fc1fb3f8fe50544644d2f08a0b24dc0355
zeruiCai <1196363729@qq.com>
2020-04-23 12:01:37 +0800

    add news

---
+++ js/news/precache-manifest.1bc80fc5a8330a4800edc91fb88c73f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9e32dfc12e9a41ef9b5d194b63a65e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f642337ff7c8a095b642",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "1f97177a44d6a875fa4c",
+    "url": "/static/css/main.2b14b002.chunk.css"
+  },
+  {
+    "revision": "f642337ff7c8a095b642",
+    "url": "/static/js/2.f3e43950.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f3e43950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f97177a44d6a875fa4c",
+    "url": "/static/js/main.7d60f633.chunk.js"
+  },
+  {
+    "revision": "961bedf27ebe1ab1816a",
+    "url": "/static/js/runtime-main.efa21d98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
