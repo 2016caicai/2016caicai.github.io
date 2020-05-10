ff0c95818ac9603257d79eb00441c9c08fb0f6f1
蔡泽锐 <caizerui@caizeruideMacBook-Pro.local>
2020-05-10 18:18:40 +0800

    :sparkles: active-app

---
+++ js/active-app/precache-manifest.7e955fb1192de166f25a25d591c38667.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087070904fb78e34542a",
+    "url": "././static/css/main.5cd23991.chunk.css"
+  },
+  {
+    "revision": "ad581116fa18b23380821c39e1298144",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13cc113b599513d66ed9",
+    "url": "./static/js/2.b579cf25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b579cf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087070904fb78e34542a",
+    "url": "./static/js/main.24c7c17c.chunk.js"
+  },
+  {
+    "revision": "3198a383c478b8c5497a",
+    "url": "./static/js/runtime-main.60bc4f9d.js"
+  },
+  {
+    "revision": "4d3b0e7794463ef9cb76fbb7fb69d830",
+    "url": "./static/media/animation.4d3b0e77.png"
+  },
+  {
+    "revision": "7b67e5c194b4f15ee7ceed9b6ceaa61a",
+    "url": "./static/media/b_egg_sprites.7b67e5c1.png"
+  },
+  {
+    "revision": "d3a6891957ac865d4fded5f82632771f",
+    "url": "./static/media/btn_sprites.d3a68919.png"
+  },
+  {
+    "revision": "2645f8528aea0a33df0f52313da3826e",
+    "url": "./static/media/index_sprites.2645f852.png"
+  },
+  {
+    "revision": "006a078e9f9686f55cbe8b5dfae02d26",
+    "url": "./static/media/s_egg_sprites.006a078e.png"
+  },
+  {
+    "revision": "69d8b876aa24b10fe11a84fececa6237",
+    "url": "./static/media/toBtn.69d8b876.png"
+  }
+]);
